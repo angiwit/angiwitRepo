f1de3a715d589b27fb483e9b2f9139eb0b59afcf
angiwit <948415694@qq.com>
2017-09-28 16:48:55 +0800

    qwtuopasdjkl

--- myFirstGitProject/src/mypack/Test.java
+++ myFirstGitProject/src/mypack/Test.java
@@ -6,7 +6,7 @@ public class Test
     public static void main(String[] args)
     {
         System.out.println("hello world niuzan");//fdsafdsafdsafds
-        System.out.println("hello world niuzan1");
+        System.out.println("hello world niuzan1");//qwertuiopasdgjkl
         System.out.println("hello world niuzan2");
         System.out.println("hello world niuzan3");
     }
