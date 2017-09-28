34de9dd1c95536ad336727ba209f160cdd3b5fed
angiwit <948415694@qq.com>
2017-09-28 16:41:30 +0800

    猜测是测试赛

--- myFirstGitProject/src/mypack/Test.java
+++ myFirstGitProject/src/mypack/Test.java
@@ -5,7 +5,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("hello world niuzan");//测试测试
+        System.out.println("hello world niuzan");
         System.out.println("hello world niuzan1");
         System.out.println("hello world niuzan2");
         System.out.println("hello world niuzan3");
