aa29f4885649d46b1f210053d88e3b261c46fcfa
angiwit <948415694@qq.com>
2017-09-28 16:44:01 +0800

    fdsafdsafdsafdsafds

--- myFirstGitProject/src/mypack/Test.java
+++ myFirstGitProject/src/mypack/Test.java
@@ -5,7 +5,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("hello world niuzan");//测试gitrebase
+        System.out.println("hello world niuzan");//fdsafdsafdsafds
         System.out.println("hello world niuzan1");
         System.out.println("hello world niuzan2");
         System.out.println("hello world niuzan3");
